c021090e30496e9eaeb773bc1d97e855e16f4885
SurajGm <62544104+suraj318@users.noreply.github.com>
2026-04-27 19:00:09 +0530

    @GeneratedValue(strategy = GenerationType.TABLE)

--- src/main/java/com/btop/entity/BaseEntity.java
+++ src/main/java/com/btop/entity/BaseEntity.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private Long id;
 
     private LocalDateTime createdAt;
